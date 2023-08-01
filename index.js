cf949ffb5e5c487c7caefa0fd7c29b016f6b43f7
Chris Leary <learymchris@gmail.com>
2023-08-01 13:31:59 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(plan = "roller-skate"){
+   return `This Saturday, I want to ${plan}!`
+    
+}
+
+function mondayWork(plan = 'go to the office') {
+    return `This Monday, I will ${plan}.`
+}
+
+// function wrapAdjective(razzle = "*", adj = 'special'){
+//     return `You are ${razzle}${adj}${razzle}!`;
+// }
+
+function wrapAdjective(razzle="*") {
+    return function(adj="special") {
+        return `You are ${razzle}${adj}${razzle}!`
+    }
+}
